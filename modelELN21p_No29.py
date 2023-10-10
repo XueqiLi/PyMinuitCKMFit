7d9af96a943979df82a61fe9fe1456ab28045995
Xueqi Li <el@xueqi.li>
2023-10-10 14:04:16 -0700

    add ENL21p model

--- modelELN21p_No29.py
+++ modelELN21p_No29.py
@@ -1,17 +1,18 @@
 import numpy as np
 from ModularForm import *
-numberOfParams = 9
+numberOfParams = 11
 def ELMatrix(params):
-    g2, g3, g4, a2, a3, a4, n2, n3, n4, tr, ti = params
+    g2, g3, g4, g5, g6, a2, a3, a4, n2, n3, n4, tr, ti = params
     t = tr + 1j * ti 
     Y2k4_t=Y2k4(t)
-    Y1k16_t=Y1k16(t)
+    Y1k20_t=Y1k20(t)
+    Y1pk20_t=Y1pk20(t)
     Y2Ik20_t=Y2Ik20(t)
     Y2IIk20_t=Y2IIk20(t)
-    return [[-(Y2k4_t[0]), Y2k4_t[1], -(g3*Y2IIk20_t[1]) - g2*Y2Ik20_t[1]], [Y2k4_t[1], Y2k4_t[0], g3*Y2IIk20_t[0] + g2*Y2Ik20_t[0]], [0, 0, g4*Y1k16_t[0]]]
+    return [[g4*Y1k20_t[0] - g2*Y2IIk20_t[0] - Y2Ik20_t[0], g3*Y1pk20_t[0] + g2*Y2IIk20_t[1] + Y2Ik20_t[1], 0], [-(g3*Y1pk20_t[0]) + g2*Y2IIk20_t[1] + Y2Ik20_t[1], g4*Y1k20_t[0] + g2*Y2IIk20_t[0] + Y2Ik20_t[0], 0], [-(g5*Y2k4_t[1]), g5*Y2k4_t[0], g6*Y1k20_t[0]]]
 
 def NLMatrix(params):
-    g2, g3, g4, a2, a3, a4, n2, n3, n4, tr, ti = params
+    g2, g3, g4, g5, g6, a2, a3, a4, n2, n3, n4, tr, ti = params
     t = tr + 1j * ti 
     Y2k4_t=Y2k4(t)
     Y2k8_t=Y2k8(t)
@@ -20,7 +21,7 @@ def NLMatrix(params):
     return [[-(Y2k4_t[0]), Y2k4_t[1], -(a4*Y2IIk20_t[1]) - a3*Y2Ik20_t[1]], [Y2k4_t[1], Y2k4_t[0], a4*Y2IIk20_t[0] + a3*Y2Ik20_t[0]], [-(a2*Y2k8_t[1]), a2*Y2k8_t[0], 0]]
 
 def NNMatrix(params):
-    g2, g3, g4, a2, a3, a4, n2, n3, n4, tr, ti = params
+    g2, g3, g4, g5, g6, a2, a3, a4, n2, n3, n4, tr, ti = params
     t = tr + 1j * ti 
     Y1k8_t=Y1k8(t)
     Y2k8_t=Y2k8(t)
