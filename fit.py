770066d87bbd1f28e7dca7ca15ef52b422477c77
Xueqi Li <el@Xueqis-Mac-mini.local>
2023-10-09 22:17:53 -0700

    have a good fit result for the example model

--- fit.py
+++ fit.py
@@ -13,8 +13,94 @@ modelName = sys.argv[1]
 modelModule = importlib.import_module(modelName)
 globals().update(vars(modelModule))
 
-# Import calculating observerable
-from CalObs import *
+class Observables:
+    def __init__(self,ELMatrix,NLMatrix,NNMatrix):
+        self.ELMatrix = ELMatrix
+        self.NLMatrix = NLMatrix
+        self.NNMatrix = NNMatrix
+
+    # Define Function used to calculate observerable
+    def GetMassDiff(self, massMatrix):
+        eigenvalues = np.linalg.eigvals(massMatrix)
+        massList = np.abs(eigenvalues)
+        massList.sort()
+
+        Dm21 = massList[1] - massList[0]
+        Dm31 = massList[2] - massList[0]
+
+        return [Dm21, Dm31]
+
+    def GetMass(self, massMatrix):
+        eigenvalues = np.linalg.eigvals(massMatrix)
+        massList = np.abs(eigenvalues)
+        massList.sort()
+
+        me, m_mu, m_tau = np.sort(massList)
+
+        return [me, m_mu, m_tau]
+
+    def GetMixing(self, umatrix):
+        s13 = umatrix[0, 2] * np.conjugate(umatrix[0, 2])
+        s12 = (umatrix[0, 1] * np.conjugate(umatrix[0, 1])) / (1 - s13)
+        s23 = (umatrix[1, 2] * np.conjugate(umatrix[1, 2])) / (1 - s13)
+        c13 = 1 - s13
+        c12 = 1 - s12
+        c23 = 1 - s23
+        return [s12,s23,s13]
+
+    def DiagHermitian(self, hermitian_matrix):
+        # Check if the input matrix is square
+        n, m = hermitian_matrix.shape
+        if n != m:
+            raise ValueError("Input matrix must be square.")
+
+        # Check if the input matrix is Hermitian (equal to its conjugate transpose)
+        if not np.allclose(hermitian_matrix, hermitian_matrix.conj().T):
+            raise ValueError("Input matrix must be Hermitian.")
+
+        # Perform eigendecomposition
+        eigenvalues, eigenvectors = np.linalg.eigh(hermitian_matrix)
+
+        # Sort eigenvalues and eigenvectors in ascending order
+        sorted_indices = np.argsort(eigenvalues)
+        sorted_eigenvalues = eigenvalues[sorted_indices]
+        sorted_eigenvectors = eigenvectors[:, sorted_indices]
+
+        # Form the diagonal matrix with sorted eigenvalues on the diagonal
+        diagonal_matrix = np.diag(sorted_eigenvalues)
+
+        # The sorted eigenvectors matrix (sorted_eigenvectors) is already a unitary matrix
+        unitary_matrix = sorted_eigenvectors
+
+        return unitary_matrix
+
+    # Calculate Observables
+    def CalObservables(self, params):
+        ELMatrixN = np.array(self.ELMatrix(params))
+        NLMatrixN = np.array(self.NLMatrix(params))
+        NNMatrixN = np.array(self.NNMatrix(params))
+        Mnu = -(np.transpose(NLMatrixN) @ np.linalg.inv(NNMatrixN) @ NLMatrixN)
+        MnuDagger = Mnu.conj().T
+        ELMatrixNDagger = ELMatrixN.conj().T
+        Mnunu = MnuDagger @ Mnu
+        Mee = ELMatrixNDagger @ ELMatrixN
+        MeeDiagMatrix = self.DiagHermitian(Mee)
+        MnunuDiagMatrix = self.DiagHermitian(Mnunu)
+        NUPMNS = np.dot(np.conj(MeeDiagMatrix.T), MnunuDiagMatrix)
+
+        Dm21, Dm31 = self.GetMassDiff(Mnunu)
+        me, m_mu, m_tau = self.GetMass(Mee)
+        m21Rm31 = Dm21/Dm31
+        mERmMu  = np.sqrt(me)/np.sqrt(m_mu)
+        mMuRMTau = np.sqrt(m_mu)/np.sqrt(m_tau)
+        s12, s23, s13 = self.GetMixing(NUPMNS)
+
+        output=np.array([np.real(x) for x in [s12, s23, s13, m21Rm31, mERmMu, mMuRMTau]], dtype=float)
+        return output
+    def __call__(self,params):
+        return self.CalObservables(params)
+
+observables=Observables(ELMatrix,NLMatrix,NNMatrix)
 
 class CostFunction:
     errordef = Minuit.LEAST_SQUARES
@@ -30,7 +116,7 @@ class CostFunction:
     def prediction(self,params):
         return self.calResult(params)
 
-costFunction = CostFunction(CalObservables,expValList,divValList)
+costFunction = CostFunction(observables,expValList,divValList)
 # Fit
 
 ## Bound and merge
@@ -41,19 +127,13 @@ tBounds = [
 ]
 
 parameterBoundsWithoutT = [commonBounds for i in range(numberOfParams)]
-parameterBounds = np.array(parameterBoundsWithoutT + tBounds)
+parameterBounds = parameterBoundsWithoutT + tBounds
 
-parameterRanges = parameterBounds[:, 1] - parameterBounds[:, 0]
+parameterRanges = np.asarray(parameterBounds)[:, 1] - np.asarray(parameterBounds)[:, 0]
 
-sizeScale = 1/10000
-mergeScale = 1/100000
+stepScale = 1/1000
 
-mergeThreshold = parameterRanges * mergeScale
-
-def are_parameters_close(params1, params2, threshold):
-    params1 = np.array(params1)
-    params2 = np.array(params2)
-    return np.all(np.abs(params1 - params2) < threshold)
+stepSize = parameterRanges * stepScale
 
 ## Actual Fit
 
