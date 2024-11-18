30579537b5ecb3c70e8f3e7f54d0dd849ac060a7
Xueqi Li <el@xueqi.li>
2024-11-18 15:28:55 -0800

    change SimpleFNQuark

--- model/SimpleFNQuark/model3020.py
+++ model/SimpleFNQuark/model3020.py
@@ -1,12 +1,12 @@
-# Input String: 3020: {{2, 1, 0}, {1, 2, 1}, {0, 2, 2}}
+# Input String: 3020: {{0, 0, 0}, {5, 4, 4}, {3, 2, 2}}
 import numpy as np
 numberOfParams = 36
 eps = 0.17
 def YuMatrix(params):
     YD11Re,YD11Im,YD12Re,YD12Im,YD13Re,YD13Im,YD21Re,YD21Im,YD22Re,YD22Im,YD23Re,YD23Im,YD31Re,YD31Im,YD32Re,YD32Im,YD33Re,YD33Im,YU11Re,YU11Im,YU12Re,YU12Im,YU13Re,YU13Im,YU21Re,YU21Im,YU22Re,YU22Im,YU23Re,YU23Im,YU31Re,YU31Im,YU32Re,YU32Im,YU33Re,YU33Im = params
-    return [[eps*(YU11Re+1j*YU11Im),(YU12Re+1j*YU12Im),eps*(YU13Re+1j*YU13Im)],[(YU21Re+1j*YU21Im),eps*(YU22Re+1j*YU22Im),(YU23Re+1j*YU23Im)],[eps*(YU31Re+1j*YU31Im),eps**2*(YU32Re+1j*YU32Im),eps*(YU33Re+1j*YU33Im)]]
+    return [[eps**5*(YU11Re+1j*YU11Im),eps**4*(YU12Re+1j*YU12Im),eps**4*(YU13Re+1j*YU13Im)],[eps**5*(YU21Re+1j*YU21Im),eps**4*(YU22Re+1j*YU22Im),eps**4*(YU23Re+1j*YU23Im)],[eps**5*(YU31Re+1j*YU31Im),eps**4*(YU32Re+1j*YU32Im),eps**4*(YU33Re+1j*YU33Im)]]
 
 def YdMatrix(params):
     YD11Re,YD11Im,YD12Re,YD12Im,YD13Re,YD13Im,YD21Re,YD21Im,YD22Re,YD22Im,YD23Re,YD23Im,YD31Re,YD31Im,YD32Re,YD32Im,YD33Re,YD33Im,YU11Re,YU11Im,YU12Re,YU12Im,YU13Re,YU13Im,YU21Re,YU21Im,YU22Re,YU22Im,YU23Re,YU23Im,YU31Re,YU31Im,YU32Re,YU32Im,YU33Re,YU33Im = params
-    return [[eps**2*(YD11Re+1j*YD11Im),(YD12Re+1j*YD12Im),(YD13Re+1j*YD13Im)],[eps*(YD21Re+1j*YD21Im),eps*(YD22Re+1j*YD22Im),eps*(YD23Re+1j*YD23Im)],[(YD31Re+1j*YD31Im),eps**2*(YD32Re+1j*YD32Im),eps**2*(YD33Re+1j*YD33Im)]]
+    return [[eps**3*(YD11Re+1j*YD11Im),eps**2*(YD12Re+1j*YD12Im),eps**2*(YD13Re+1j*YD13Im)],[eps**3*(YD21Re+1j*YD21Im),eps**2*(YD22Re+1j*YD22Im),eps**2*(YD23Re+1j*YD23Im)],[eps**3*(YD31Re+1j*YD31Im),eps**2*(YD32Re+1j*YD32Im),eps**2*(YD33Re+1j*YD33Im)]]
 
